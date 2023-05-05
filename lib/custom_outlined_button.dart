7ae336793a104212e9a6e51854616cd73ff4582e
shivanshusrivastava <shivanshu.srivastava@dhiwise.com>
2023-05-05 09:25:08 +0530

    Removed unused import

--- lib/custom_outlined_button.dart
+++ lib/custom_outlined_button.dart
@@ -1,5 +1,4 @@
 import 'package:demo_class_project/base_button.dart';
-import 'package:demo_class_project/button_theme_helper.dart';
 import 'package:flutter/material.dart';
 
 /// Button which extends [BaseButton] class and returns an returns an [OutlinedButton[ widget
