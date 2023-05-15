a3a55d132a9cbc2e585e6f4b3375f7d382b50256
shivanshusrivastava <shivanshu.srivastava@dhiwise.com>
2023-05-15 08:56:34 +0530

    Custom Checkbox and radio button

---
+++ lib/custom_checkbox.dart
@@ -0,0 +1,110 @@
+import 'package:demo_class_project/app_color_helper.dart';
+import 'package:demo_class_project/decoration_helper.dart';
+import 'package:demo_class_project/text_theme_helper.dart';
+import 'package:flutter/material.dart';
+
+class CustomCheckbox extends StatelessWidget {
+  CustomCheckbox({
+    super.key,
+    this.alignment,
+    this.isRightCheck = false,
+    this.iconSize,
+    this.value = false,
+    this.onChange,
+    this.text,
+    this.width,
+    this.margin,
+    this.padding,
+  });
+
+  final Alignment? alignment;
+
+  final bool? isRightCheck;
+
+  final double? iconSize;
+
+  bool? value;
+
+  final Function(bool)? onChange;
+
+  final String? text;
+
+  final double? width;
+
+  final EdgeInsetsGeometry? margin;
+
+  final EdgeInsetsGeometry? padding;
+
+  @override
+  Widget build(BuildContext context) {
+    return alignment != null
+        ? Align(
+            alignment: alignment ?? Alignment.center,
+            child: _buildCheckboxWidget,
+          )
+        : _buildCheckboxWidget;
+  }
+
+  Widget get _buildCheckboxWidget {
+    return InkWell(
+      onTap: () {
+        value = !(value!);
+        onChange!(value!);
+      },
+      child: Container(
+        width: width,
+        margin: margin ?? EdgeInsets.zero,
+        padding: padding,
+        decoration: DecorationHelper.roundGreenGradient,
+        child: isRightCheck! ? rightSideCheckbox : leftSideCheckbox,
+      ),
+    );
+  }
+
+  Widget get rightSideCheckbox => Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(
+              right: 8,
+            ),
+            child: textWidget,
+          ),
+          checkboxWidget,
+        ],
+      );
+
+  Widget get leftSideCheckbox => Row(
+        children: [
+          checkboxWidget,
+          Padding(
+            padding: const EdgeInsets.only(
+              left: 8,
+            ),
+            child: textWidget,
+          ),
+        ],
+      );
+
+  Widget get textWidget => Text(
+        text ?? "",
+        textAlign: TextAlign.center,
+        style: TextThemeHelper.bodySmallBlack400,
+      );
+
+  Widget get checkboxWidget => SizedBox(
+        height: iconSize,
+        width: iconSize,
+        child: Checkbox(
+          value: value ?? false,
+          onChanged: (value) {
+            onChange!(value!);
+          },
+          checkColor: ColorConstant.green5,
+          visualDensity: const VisualDensity(
+            vertical: -4,
+            horizontal: -4,
+          ),
+        ),
+      );
+}
---
+++ lib/custom_radio_button.dart
@@ -0,0 +1,104 @@
+import 'package:demo_class_project/text_theme_helper.dart';
+import 'package:flutter/material.dart';
+
+class CustomRadioButton extends StatelessWidget {
+  CustomRadioButton(
+      {super.key,
+      this.alignment,
+      this.onChange,
+      this.isRightCheck = false,
+      this.iconSize,
+      this.value,
+      this.groupValue,
+      this.text,
+      this.width,
+      this.margin});
+
+  final Alignment? alignment;
+
+  final Function(String)? onChange;
+
+  final bool? isRightCheck;
+
+  final double? iconSize;
+
+  String? value;
+
+  final String? groupValue;
+
+  final String? text;
+
+  final double? width;
+
+  final EdgeInsetsGeometry? margin;
+
+  @override
+  Widget build(BuildContext context) {
+    return alignment != null
+        ? Align(
+            alignment: alignment ?? Alignment.center,
+            child: radioButtonWidget,
+          )
+        : radioButtonWidget;
+  }
+
+  Widget get radioButtonWidget => InkWell(
+        onTap: () {
+          onChange!(value!);
+        },
+        child: Container(
+          width: width,
+          margin: margin ?? EdgeInsets.zero,
+          child: isRightCheck! ? rightSideRadio : leftSideRadio,
+        ),
+      );
+
+  Widget get rightSideRadio => Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(
+              right: 8,
+            ),
+            child: textWidget,
+          ),
+          radioWidget,
+        ],
+      );
+
+  Widget get leftSideRadio => Row(
+        children: [
+          radioWidget,
+          Padding(
+            padding: const EdgeInsets.only(
+              left: 8,
+            ),
+            child: textWidget,
+          ),
+        ],
+      );
+
+  Widget get textWidget => Text(
+        text ?? "",
+        textAlign: TextAlign.center,
+        style: TextThemeHelper.displaySmallGreen600,
+      );
+
+  Widget get radioWidget {
+    return SizedBox(
+      height: iconSize,
+      width: iconSize,
+      child: Radio<String>(
+        value: value ?? "",
+        groupValue: groupValue,
+        onChanged: (value) {
+          onChange!(value!);
+        },
+        visualDensity: const VisualDensity(
+          vertical: -4,
+          horizontal: -4,
+        ),
+      ),
+    );
+  }
+}
